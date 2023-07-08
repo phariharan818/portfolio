99e78784090c98c8d435ad410a36f8d84102e17d
phariharan818 <priya.hariharan818@gmail.com>
2023-07-08 15:30:56 +0000

    redeploy script

---
+++ redeploy-site.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+tmux kill-session -a
+
+cd ~/priya-portfolio
+
+git fetch && git reset origin/main --hard
+
+source python3-virtualenv/bin/activate
+
+pip install -r requirements.txt
+
+tmux new-session -d -s flask-session 'python app.py'
