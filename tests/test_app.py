94bfa49c33a68392a03e9d6ed7f014c5760b365a
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-07-20 13:35:05 -0700

    add additional tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,8 +15,8 @@ class AppTestCase(unittest.TestCase):
 
         html = response.get_data(as_text=True)
         assert "<title>Priya Hariharan</title>" in html
-
-        # TODO Add more tests relating to index
+        assert "Hi! My name is Priya Hariharan" in html
+        assert '<a href="https://github.com/phariha/"' in html
 
     def test_timeline(self):
         response = self.client.get("/api/timeline_post")
@@ -26,8 +26,28 @@ class AppTestCase(unittest.TestCase):
         json = response.get_json()
         assert len(json["timeline_posts"]) == 0
 
-        # TODO Add more tests relating to GET and POST /api/timeline_post
-        # TODO Add more tests relating to /timeline
+        response = self.client.get("/timeline")
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+
+        assert "Create Post" in html
+        assert "Timeline Posts" in html
+
+        data = {
+            "name": "John Doe",
+            "email": "john@mail.com",
+            "content": "Hello world, I'm John!",
+        }
+        response = self.client.post("/api/timeline_post", data=data)
+        assert response.status_code == 200
+        assert response.is_json
+
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
+
+        json = response.get_json()
+        assert len(json["timeline_posts"]) == 1
 
     def test_malformed_timeline_post(self):
         response = self.client.post(
