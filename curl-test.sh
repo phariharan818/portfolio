b7f85918f7576959f06679e83b6c8a504f4dc82b
Priya Hariharan <133527260+phariharan818@users.noreply.github.com>
2023-08-08 15:42:38 -0700

    Revert "Run Test Fix"

--- curl-test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl --request POST 127.0.0.1:5000/api/timeline_post -d 'name=Priya&email=phariha@gmail.com&content=testing 123'
-curl 127.0.0.1:5000/api/timeline_post
-curl --request DELETE 127.0.0.1:5000/api/timeline_post -d 'name=Priya&email=phariha@gmail.com&content=testing 123'
-curl 127.0.0.1:5000/api/timeline_post
--- redeploy-site.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-cd ~/priya-portfolio
-
-git fetch && git reset origin/main --hard
-
-docker compose -f docker-compose.prod.yml down
-
-docker compose -f docker-compose.prod.yml up -d --build
